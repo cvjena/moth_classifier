83c017de52e4bae9ff9eba95ab564a804ee60c36
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2021-03-31 13:38:34 +0200

    refactored parser module a bit

--- moth_classifier/utils/parser.py
+++ moth_classifier/utils/parser.py
@@ -3,6 +3,31 @@ from cvargparse import Arg
 from cvargparse import BaseParser
 from cvfinetune.parser import FineTuneParser
 
+def add_args(parser: BaseParser):
+
+	parser.add_args([
+		Arg("--update_size", type=int, default=-1,
+			help="if positive, MiniBatchUpdater is used." + \
+			"It accumulates gradients over the training and " + \
+			"updates the weights only if the update size is exceeded"),
+
+		Arg("--mpi", action="store_true",
+			help="Indicates that OpenMPI is used!"),
+
+	], group_name="Training arguments")
+
+	parser.add_args([
+		Arg("--concat_features", action="store_true",
+			help="If set, concatenate part features instead of averaging."),
+	], group_name="Model arguments")
+
+	parser.add_args([
+		Arg("--no_sacred", action="store_true",
+			help="do save outputs to sacred"),
+
+	], group_name="Sacred arguments")
+
+
 def parse_args(args=None, namespace=None):
 	main_parser = BaseParser()
 
@@ -17,26 +42,6 @@ def parse_args(args=None, namespace=None):
 		help="Starts moth classifier training",
 		parents=[_common_parser])
 
-
-	parser.add_args([
-		Arg("--update_size", type=int, default=-1,
-			help="if positive, MiniBatchUpdater is used." + \
-			"It accumulates gradients over the training and " + \
-			"updates the weights only if the update size is exceeded"),
-
-		Arg("--mpi", action="store_true",
-			help="Indicates that OpenMPI is used!"),
-
-	], group_name="Training arguments")
-	parser.add_args([
-		Arg("--concat_features", action="store_true",
-			help="If set, concatenate part features instead of averaging."),
-	], group_name="Model arguments")
-
-	parser.add_args([
-		Arg("--no_sacred", action="store_true",
-			help="do save outputs to sacred"),
-
-	], group_name="Sacred arguments")
+	add_args(parser)
 
 	return main_parser.parse_args(args=args, namespace=namespace)

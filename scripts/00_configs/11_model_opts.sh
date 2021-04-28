ca35d56d161bbc98adac0be694501d128b6b5726
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2021-04-28 18:47:48 +0200

    added PRE_TRAINING option in the shell scripts

--- scripts/00_configs/11_model_opts.sh
+++ scripts/00_configs/11_model_opts.sh
@@ -3,6 +3,8 @@ FINAL_POOLING=${FINAL_POOLING:-g_avg}
 # resnet inception inception_tf [vgg]
 MODEL_TYPE=${MODEL_TYPE:-cvmodelz.InceptionV3}
 
+PRE_TRAINING=${PRE_TRAINING:-inat}
+
 case $MODEL_TYPE in
 	"cvmodelz.InceptionV3" )
 		PARTS_INPUT_SIZE=299
@@ -31,3 +33,4 @@ OPTS="${OPTS} --input_size ${INPUT_SIZE}"
 OPTS="${OPTS} --parts_input_size ${PARTS_INPUT_SIZE}"
 OPTS="${OPTS} --pooling ${FINAL_POOLING}"
 OPTS="${OPTS} --load_strict"
+OPTS="${OPTS} --pre_training ${PRE_TRAINING}"

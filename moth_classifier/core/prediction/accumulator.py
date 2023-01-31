a59a1392baee1346d4ea2e080f96b49e04b41293
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2023-01-31 10:26:32 +0100

    removed unused code

--- moth_classifier/core/prediction/accumulator.py
+++ moth_classifier/core/prediction/accumulator.py
@@ -133,6 +133,34 @@ def _calc_scores(pred, gt, *, beta: float = 1.0) -> Scores:
 	)
 
 
+def argmax(arr, axis=1, *, mask=None):
+	if mask is None:
+		return np.argmax(arr, axis=1)
+
+	# fill the non-mask entries with the smallest value
+	arr[~mask] = arr.min()
+	return np.argmax(arr, axis=1)
+
+def h_argmax(arr, hierarchy: Hierarchy, *, available_uuids = set()):
+	""" Hierarchical argmax. """
+
+	probs = chainer.as_array(F.sigmoid(arr))
+	probs_per_node = hierarchy.deembed_dist(probs)
+
+	if available_uuids:
+		# remove all probabilities that are not available
+		probs_per_node = [
+			list(filter(lambda tup: tup[0] in available_uuids, probs))
+			for probs in probs_per_node
+		]
+
+	# sort by the de-embedded probabilities and
+	# return the class uuid of the one with the max prob
+	preds = [sorted(probs, key=lambda tup: tup[1], reverse=True)[0][0]
+		for probs in probs_per_node]
+
+	return preds
+
 POOL = None
 
 class PredictionAccumulator:
@@ -192,50 +220,6 @@ class PredictionAccumulator:
 			self._gt += gt
 		else:
 			self._gt += [_to_cpu(gt, np.int32)]
-
-	def calc_hierarchical_metrics(self, *, only_available: bool = True, beta: int = 1):
-		"""
-			partially from https://github.com/cabrust/chia/blob/main/chia/components/evaluators/hierarchical.py#L31
-		"""
-		logits, true = self.reset()
-
-		# transform GT labels to original label uid
-		hc_orig_gt = np.fromiter(map(self.hierarchy.label_transform, true), dtype=true.dtype)
-
-		pred = chainer.as_array(F.sigmoid(logits))
-		deembed_pred_dist = self.hierarchy.deembed_dist(pred)
-
-		if only_available:
-			_available_leaves = np.unique(hc_orig_gt)
-			idx_of_available = set(_available_leaves)
-
-			for label in _available_leaves:
-				idx_of_available |= set(nx.ancestors(self.hierarchy.graph, label))
-
-			deembed_pred_dist = [
-				list(filter(lambda tup: tup[0] in idx_of_available, preds))
-				for preds in deembed_pred_dist
-			]
-
-		# argmax
-		predictions = np.array([
-			sorted(dist, key=lambda tup: tup[1], reverse=True)[0][0]
-				for dist in deembed_pred_dist
-		])
-
-		scores = _calc_h_scores(predictions, hc_orig_gt,
-			beta=beta, graph=self.hierarchy.graph)
-
-		self._metrics = {
-			"accuracy": scores.accuracy,
-			"precision": scores.precision,
-			"recall": scores.recall,
-			f"f{beta}_score": scores.f_score,
-
-		}
-
-		return self._metrics
-
 
 	def calc_metrics(self, *, only_available: bool = True, beta: int = 1):
 		if self._metrics is not None:
@@ -326,123 +310,3 @@ class PredictionAccumulator:
 			true_uids = self._to_uids(true)
 			pred_uids = self._to_uids(preds)
 			return pred_uids, true_uids
-
-
-def argmax(arr, axis=1, *, mask=None):
-	if mask is None:
-		return np.argmax(arr, axis=1)
-
-	# fill the non-mask entries with the smallest value
-	arr[~mask] = arr.min()
-	return np.argmax(arr, axis=1)
-
-def h_argmax(arr, hierarchy: Hierarchy, *, available_uuids = set()):
-	""" Hierarchical argmax. """
-
-	probs = chainer.as_array(F.sigmoid(arr))
-	probs_per_node = hierarchy.deembed_dist(probs)
-
-	if available_uuids:
-		# remove all probabilities that are not available
-		probs_per_node = [
-			list(filter(lambda tup: tup[0] in available_uuids, probs))
-			for probs in probs_per_node
-		]
-
-	# sort by the de-embedded probabilities and
-	# return the class uuid of the one with the max prob
-	preds = [sorted(probs, key=lambda tup: tup[1], reverse=True)[0][0]
-		for probs in probs_per_node]
-
-	return preds
-
-
-	# def calc_metrics(self, *, only_available: bool = True, beta: int = 1):
-
-	# 	if self._metrics is not None:
-	# 		return self._metrics
-
-	# 	if self.use_hc:
-	# 		return self.calc_hierarchical_metrics(only_available=only_available, beta=beta)
-
-	# 	logits, true = self.reset()
-	# 	n_cls = max(logits.shape[1], true.max())
-	# 	if only_available:
-	# 		_logits = np.full_like(logits, fill_value=logits.min())
-	# 		available = np.unique(true)
-	# 		_logits[:, available] = logits[:, available]
-	# 		logits = _logits
-
-	# 	pred = logits.argmax(axis=1)
-
-	# 	counts = np.bincount(true, minlength=n_cls + 1)[:n_cls]
-	# 	relevant = np.bincount(pred, minlength=n_cls + 1)[:n_cls]
-
-	# 	tp_mask = np.where(pred == true, true, n_cls)
-	# 	tp = np.bincount(tp_mask, minlength=n_cls + 1)[:n_cls]
-
-	# 	precision = np.zeros_like(tp, dtype=np.float32)
-	# 	recall = np.zeros_like(tp, dtype=np.float32)
-	# 	fbeta_score = np.zeros_like(tp, dtype=np.float32)
-
-	# 	count_mask = counts != 0
-	# 	relev_mask = relevant != 0
-
-	# 	precision[relev_mask] = tp[relev_mask] / relevant[relev_mask]
-	# 	recall[count_mask] = tp[count_mask] / counts[count_mask]
-
-	# 	# F-Measure
-	# 	beta_square = beta ** 2
-	# 	numerator = (1 + beta_square) * precision * recall
-	# 	denominator = beta_square * precision + recall
-	# 	mask = denominator != 0
-	# 	fbeta_score[mask] = numerator[mask] / denominator[mask]
-
-	# 	prec = precision[count_mask]
-	# 	rec = recall[count_mask]
-	# 	fbeta = fbeta_score[count_mask]
-
-	# 	self._metrics = {
-	# 		"accuracy": np.mean(pred == true),
-	# 		"precision": np.nanmean(prec),
-	# 		"recall": np.nanmean(rec),
-	# 		f"f{beta}_score": np.nanmean(fbeta),
-	# 	}
-
-	# 	if -1 not in [self.few_shot_count, self.many_shot_count]:
-
-	# 		fsc = self.few_shot_count
-	# 		msc = self.many_shot_count
-	# 		_counts = counts[count_mask]
-	# 		fs_mask = _counts < fsc
-	# 		ms_mask = _counts > msc
-	# 		mds_mask = np.logical_and(~fs_mask, ~ms_mask)
-
-	# 		if fs_mask.sum():
-	# 			self._metrics.update({
-	# 				"few_shot_cls_count": fs_mask.sum(),
-	# 				"few_shot_count": _counts[fs_mask].sum(),
-	# 				f"precision/few-shot@{fsc}": np.nanmean(prec[fs_mask]),
-	# 				f"recall/few-shot@{fsc}": np.nanmean(rec[fs_mask]),
-	# 				f"f{beta}_score/few-shot@{fsc}": np.nanmean(fbeta[fs_mask]),
-	# 			})
-
-	# 		if mds_mask.sum():
-	# 			self._metrics.update({
-	# 				"med_shot_cls_count": mds_mask.sum(),
-	# 				"med_shot_count": _counts[mds_mask].sum(),
-	# 				f"precision/med-shot@{fsc}-{msc}": np.nanmean(prec[mds_mask]),
-	# 				f"recall/med-shot@{fsc}-{msc}": np.nanmean(rec[mds_mask]),
-	# 				f"f{beta}_score/med-shot@{fsc}-{msc}": np.nanmean(fbeta[mds_mask]),
-	# 				})
-
-	# 		if ms_mask.sum():
-	# 			self._metrics.update({
-	# 				"many_shot_cls_count": ms_mask.sum(),
-	# 				"many_shot_count": _counts[ms_mask].sum(),
-	# 				f"precision/many-shot@{msc}": np.nanmean(prec[ms_mask]),
-	# 				f"recall/many-shot@{msc}": np.nanmean(rec[ms_mask]),
-	# 				f"f{beta}_score/many-shot@{msc}": np.nanmean(fbeta[ms_mask]),
-	# 			})
-
-	# 	return self._metrics
